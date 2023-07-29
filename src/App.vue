ce5b943d01c0db630cfb4c75bb06aed704453f7e
Brandon Lee <brandonlee781@gmail.com>
2023-07-28 19:21:48 -0500

    style changes

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,7 @@ onClickOutside(mobileMenu, () => {
       />
     </div>
 
-    <div v-if="groupTab === 'qualified'" class="grid grid-cols-[3fr,1fr] md:grid-cols-[3fr,1fr,3fr,1fr] gap-2 justify-between">
+    <div v-if="groupTab === 'qualified'" class="grid grid-cols-[3fr,1fr] md:grid-cols-[3fr,1fr,3fr,1fr] gap-2 justify-between mt-2 md:mt-0">
       <TeamGroup name="Americas" :teams="['evil_geniuses', 'loud', 'nrg']" />
       <TeamGroup name="Americas LCQ" :teams="['kru']" />
       <TeamGroup name="EMEA" :teams="['fnatic', 'fut', 'navi', 'team_liquid']" class="col-span-2" />
@@ -62,7 +62,7 @@ onClickOutside(mobileMenu, () => {
       <TeamGroup name="China" :teams="['bilibili', 'edward_gaming', 'fpx']" />
       <TeamGroup name="EMEA LCQ" :teams="['giants']" />
     </div>
-    <button v-if="groupTab === 'qualified'" @click="groupTab = 'groups'" class="mt-2 px-3 py-2 hover:bg-[#161616]/40">
+    <button v-if="groupTab === 'qualified'" @click="groupTab = 'groups'" class="mt-2 px-3 py-2 hover:bg-[#161616]/40 md:mt-0">
       Show Team Groups
     </button>
     <div v-if="groupTab === 'groups'" class="grid grid-cols-2 md:flex gap-2 justify-between">
