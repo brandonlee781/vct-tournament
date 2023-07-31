e201f85d4b650f76bc7e4331b8876b558e092a9d
Brandon Lee <brandonlee781@gmail.com>
2023-07-30 20:01:27 -0500

    Spacing

--- src/components/TeamGroup/PlayGroups.vue
+++ src/components/TeamGroup/PlayGroups.vue
@@ -3,7 +3,7 @@ defineEmits<{ click: [] }>()
 </script>
 
 <template>
-  <div class="grid grid-cols-2 md:flex gap-2 justify-between mt-2 md:0">
+  <div class="grid grid-cols-2 md:flex gap-2 justify-between mt-2">
     <TeamGroup name="Group A" :teams="['paper_rex', 'kru', 'edward_gaming', 'giants']" :cols="2" />
     <TeamGroup name="Group B" :teams="['evil_geniuses', 'fpx', 'fut', 't1']" :cols="2" />
     <TeamGroup name="Group C" :teams="['fnatic', 'zeta_division', 'nrg', 'bilibili']" :cols="2" />
--- src/components/TeamGroup/QualifiedGroups.vue
+++ src/components/TeamGroup/QualifiedGroups.vue
@@ -3,7 +3,7 @@ defineEmits<{ click: [] }>()
 </script>
 
 <template>
-  <div class="qualified grid grid-cols-[3fr,1fr] md:grid-cols-[3fr,1fr,3fr,1fr] gap-2 justify-between mt-2 md:mt-0">
+  <div class="qualified grid grid-cols-[3fr,1fr] md:grid-cols-[3fr,1fr,3fr,1fr] gap-2 justify-between mt-2">
     <TeamGroup name="Americas" :teams="['evil_geniuses', 'loud', 'nrg']" />
     <TeamGroup name="Americas LCQ" :teams="['kru']" />
     <TeamGroup name="EMEA" :teams="['fnatic', 'fut', 'navi', 'team_liquid']" class="col-span-2" />
@@ -12,7 +12,7 @@ defineEmits<{ click: [] }>()
     <TeamGroup name="China" :teams="['bilibili', 'edward_gaming', 'fpx']" />
     <TeamGroup name="EMEA LCQ" :teams="['giants']" />
   </div>
-  <button @click="$emit('click')" class="groups mt-2 px-3 py-2 hover:bg-[#161616]/40">
+  <button @click="$emit('click')" class="groups px-3 py-2 hover:bg-[#161616]/40">
     Show Group Stage
   </button>
 </template>
